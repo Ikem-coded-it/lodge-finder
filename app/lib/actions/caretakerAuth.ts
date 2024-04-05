76d1b06c264e28227d6f39df78e78462394f7e5d
Ikemefuna Onubogu <ikemefunaonubogu55@gmail.com>
2024-04-05 09:26:30 +0100

    fixed deployment bug

--- app/lib/actions/caretakerAuth.ts
+++ app/lib/actions/caretakerAuth.ts
@@ -3,7 +3,6 @@
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import { Caretaker } from '../definitions';
 
 export async function signupCaretaker(formData: FormData) {
 
