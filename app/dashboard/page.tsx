6a243c1fbf96f4b2cc7bc7c3c8049c755c2bd6cf
ikemefuna <ikemefunaonubogu55@gmail.com>
2024-09-25 00:10:11 +0100

    updates

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,25 @@
-"use client";
+// "use client";
 
-import { ReactNode, useEffect } from "react";
-import { fetchAllCaretakers } from "../lib/data/caretaker";
+// import { ReactNode, useEffect } from "react";
+// import { fetchAllCaretakers } from "../lib/data/caretaker";
 
-export default function Page({ children }: { children: ReactNode }) {
-  useEffect(() => {
-    const fetchCaretaker = async () => {
-      try {
-        const res = await fetchAllCaretakers();
+// export default function Page({ children }: { children: ReactNode }) {
+//   useEffect(() => {
+//     const fetchCaretaker = async () => {
+//       try {
+//         const res = await fetchAllCaretakers();
 
-        if (res instanceof Error) {
-          console.log("dashboard page error", res.message);
-        }
-      } catch (error: any) {
-        console.log("dasboard catch error", error.message);
-      }
-    };
+//         if (res instanceof Error) {
+//           console.log("dashboard page error", res.message);
+//         }
+//       } catch (error: any) {
+//         console.log("dasboard catch error", error.message);
+//       }
+//     };
 
-    fetchCaretaker();
-  }),
-    [];
+//     fetchCaretaker();
+//   }),
+//     [];
 
-  return <></>;
-}
+//   return <></>;
+// }
